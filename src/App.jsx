4a2d18c5da678a82df391f338adbd98bb7e7328b
Ridhima17-9 <23bai71451@cuchd.in>
2026-02-05 11:02:43 +0530

    home fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/home";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import { Router, Routes, Route } from "react-router-dom";
@@ -12,7 +11,7 @@ const App = () => {
        <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Projects/>} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
